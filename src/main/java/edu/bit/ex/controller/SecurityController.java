402d86f202e8820b9b4b82becdac99975152251a
nqwrt <nqwrt@ymail.com>
2021-06-06 21:59:11 +0900

    step - 2 설명

---
+++ src/main/java/edu/bit/ex/controller/SecurityController.java
@@ -0,0 +1,26 @@
+package edu.bit.ex.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Controller
+@RequestMapping("/security/*")
+public class SecurityController {
+
+	@GetMapping("/all")
+	public void doAll() {
+		log.info("do all can access everybody");
+	}
+
+	@GetMapping("/member")
+	public void doMember() {
+
+		log.info("logined member");
+	}
+}
