be316cd1e9301c07ac6b9ef0fff0abc54b721456
nqwrt <nqwrt@ymail.com>
2021-06-07 21:22:59 +0900

    step6_로그인페이지_커스텀화

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -8,12 +8,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j;
 
 /**
  * Handles requests for the application home page.
  */
+@Log4j
 @Controller
 public class HomeController {
 	
@@ -34,6 +38,12 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@GetMapping("/login/loginForm")
+	public String loginForm() {
+		log.info("Welcome Login Form");
+		return "login/loginForm2";
 	}
 	
 }
